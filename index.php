d6f19696586a9579eb5799274df45190d6f33386
Python The Hack <lrulvoni@gmail.com>
2022-05-20 12:49:50 +0200

    update cart box

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce - Shop</title>
+    <!-- Link CSS --->
+
+    <link rel="stylesheet" href="css/style.css">
+    <!-- box icons -->
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+
+    
+</head>
+<body>
+
+<!--Header-->
+<header>
+    <!-- Nav -->
+    <div class="nav container">
+        <a href="#" class="logo">Ecommerce</a>
+        <!-- Cart Incon -->
+        <i class='bx bx-shopping-bag' id="cart-icon"></i>
+    </div>
+</header>
+<!-- Shop -->
+<section class="shop container">
+    <h2 class="section-title">Prodotti</h2>
+    <!-- COntent -->
+    <div class="shop-content">
+        <!-- Box 1 --> 
+        <div class="product-box">
+            <img src="img/leather.jpg" alt="" class="product-img">
+            <h2 class="product-title">Titolo del prodotto</h2>
+            <span class="price">€ 40</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+         <!-- Box 2 --> 
+         <div class="product-box">
+            <img src="img/white-high-top-sneakers-unisex-footwear-fashion.jpg" alt="" class="product-img">
+            <h2 class="product-title">Titolo del prodotto</h2>
+            <span class="price">€ 40</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+         <!-- Box 3 --> 
+         <div class="product-box">
+            <img src="img/men-shoes.jpg" alt="" class="product-img">
+            <h2 class="product-title">Titolo del prodotto</h2>
+            <span class="price">€ 90</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+         <!-- Box 4 --> 
+         <div class="product-box">
+            <img src="img/young-female-volleyball-player-isolated-on-white-studio-background-woman-in-sport-s-equipment-and-shoes-or-sneakers-training-and-practicing-concept-of-sport-healthy-lifestyle-motion-and-movement.jpg" alt="" class="product-img">
+            <h2 class="product-title">Titolo del prodotto</h2>
+            <span class="price">€ 40</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+         <!-- Box 5 --> 
+         <div class="product-box">
+            <img src="img/leather.jpg" alt="" class="product-img">
+            <h2 class="product-title">Titolo del prodotto</h2>
+            <span class="price">€ 40</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+
+         <!-- Box 6 --> 
+         <div class="product-box">
+            <img src="img/leather.jpg" alt="" class="product-img">
+            <h2 class="product-title">Titolo del prodotto</h2>
+            <span class="price">€ 40</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+         <!-- Box 7 --> 
+         <div class="product-box">
+            <img src="img/sport-shoes-for-running.jpg" alt="" class="product-img">
+            <h2 class="product-title">Titolo del prodotto</h2>
+            <span class="price">€ 40</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+         <!-- Box 8 --> 
+         <div class="product-box">
+            <img src="img/isolated-opened-black-t-shirt.jpg" alt="" class="product-img">
+            <h2 class="product-title">Titolo del prodotto</h2>
+            <span class="price">€ 40</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+</div>
+</section>
+
+    
+</body>
+</html>
